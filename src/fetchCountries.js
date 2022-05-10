f6e6a3105778b6999f80dec11f43755fb637c7ac
Aleksandr Kravchuk <alexandrokrk@gmail.com>
2022-05-10 19:10:30 +0300

    1

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,5 +1,23 @@
- 
-
-export function fetchCountries(name) {
+// const country = fetch('https://restcountries.com/v3.1/name/peru')
+//     .then((response) =>   response.json())
+// .then ((name)=>console.log(name)) 
     
-};
+
+// console.log(country)
+
+
+
+
+
+
+
+
+
+
+
+  fetch('https://restcountries.com/v3.1/all')
+    .then((response) =>   response.json())
+        .then((nameArray) => console.log(nameArray)  ) 
+        
+    .catch((error)=> console.log('error'));
+    
