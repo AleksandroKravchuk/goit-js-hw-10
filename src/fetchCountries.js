d6133c5e63286c62cfaf467c7da645f8e27f868d
Aleksandr Kravchuk <alexandrokrk@gmail.com>
2022-05-11 22:48:52 +0300

    1

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,16 +1,64 @@
 
 
 
-
-function fetchCountries(name) {
-  fetch('https://restcountries.com/v2/all?fields=flag,name,capital,population,languages')
-    .then((response) =>   response.json())
-    .then((nameArrow) => {
-   
-      const ty = nameArrow.find(item => item.name.toLowerCase() === name.toLowerCase())
-    console.log(ty )}) 
-        
-    .catch((error)=> console.log('error'));} 
+// function fetchCountries(name) {
+//  return fetch('https://restcountries.com/v2/all?fields=flag,name,capital,population,languages')
+//     .then((response) =>   response.json())
+// }; 
     
-fetchCountries("ukraine");
+//  fetchCountries().then(renderListCantries) .catch((error)=> console.log('error'));
+// fetchCountries(name).then( renderListCards) .catch((error)=> console.log('error'));
+// function renderListCantries (nameArrow) {
+//   console.log(nameArrow)
+// };
 
+// function renderListCards(name, nameArrow) {
+//    const card = nameArrow.find(item => item.name.toLowerCase() === name.toLowerCase())
+//     console.log(card )
+  
+// }
+
+
+const fetchCountries =(name)=> {
+ return fetch(`https://restcountries.com/v3.1/name/${name}`)
+    .then((response) =>   response.json())
+}; 
+    
+//  fetchCountries('Peru').then(renderListCantries) .catch((error)=> console.log('error'));
+// fetchCountries(name).then( renderListCards) .catch((error)=> console.log('error'));
+// function renderListCantries(nameArrow) {
+//   const countryObject = nameArrow[0]
+  
+//   const capital = countryObject.capital[0];
+//   const population = city.population;
+//   const languages = Object.values(city.languages).join(', ');
+//   const name = city.name.official;
+//   const flag = city.flags.svg;
+
+//   console.log(capital);
+//   console.log(city);
+//   console.log( name)
+//   return city({ capital }) => {
+
+//         return `
+//  <h2>  <img
+//       src="${flag}"
+//       alt="${name}"
+//     />${name} </h2>
+//   <p>Capital:'${ capital }'</p>
+//   <p>Population:${population}</p>
+//   <p>Languages:${languages}</p>
+
+//     `
+//     }).join('');
+
+// };
+
+// refs.countryList.insertAdjacentHTML('beforeend',renderListCantries)
+
+
+// function renderListCards(name, nameArrow) {
+//    const card = nameArrow.find(item => item.name.toLowerCase() === name.toLowerCase())
+//     console.log(card )
+  
+// }
--- src/index.js
+++ src/index.js
@@ -1,3 +1,53 @@
 import './css/styles.css';
-import countries from './fetchCountries'
+import fetchCountries from './fetchCountries'
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+var debounce = require('lodash.debounce');
+
+
 const DEBOUNCE_DELAY = 300;
+const refs = {
+  onInput: document.querySelector('#search-box'),
+  countryList: document.querySelector('.country-list'),
+  countryInfo: document.querySelector('.country-info'),
+}
+
+
+
+
+refs.onInput.addEventListener('input', _.debounce(onInput,DEBOUNCE_DELAY));
+
+function onInput(evt) {
+  console.log(evt.currentTarget.value);
+  // return (Notify.info("Too many matches found. Please enter a more specific name."));
+   // console.log(Notify.failure("Oops, there is no country with that name")) 
+}
+
+//  fetchCountries('Peru').then(renderListCantries) .catch((error)=> console.log('error'));
+
+//  function renderListCantries(nameArrow) {
+//   const countryObject = nameArrow[0]
+  
+//   const capital = countryObject.capital[0];
+//   const population = city.population;
+//   const languages = Object.values(city.languages).join(', ');
+//   const name = city.name.official;
+//   const flag = city.flags.svg;
+
+//   console.log(capital);
+//   console.log(city);
+//   console.log( name)
+//   return city({ capital }) => {
+
+//         return `
+//  <h2>  <img
+//       src="${flag}"
+//       alt="${name}"
+//     />${name} </h2>
+//   <p>Capital:'${ capital }'</p>
+//   <p>Population:${population}</p>
+//   <p>Languages:${languages}</p>
+
+//     `
+//     }).join('');
+
+// };
