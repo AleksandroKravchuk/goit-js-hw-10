cd6d57bf5b6d904df1bc11854bc382615a5821fc
Aleksandr Kravchuk <alexandrokrk@gmail.com>
2022-05-12 19:10:10 +0300

    2

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,64 +1,8 @@
+const BASE_URL = `https://restcountries.com`;
 
-
-
-// function fetchCountries(name) {
-//  return fetch('https://restcountries.com/v2/all?fields=flag,name,capital,population,languages')
-//     .then((response) =>   response.json())
-// }; 
-    
-//  fetchCountries().then(renderListCantries) .catch((error)=> console.log('error'));
-// fetchCountries(name).then( renderListCards) .catch((error)=> console.log('error'));
-// function renderListCantries (nameArrow) {
-//   console.log(nameArrow)
-// };
-
-// function renderListCards(name, nameArrow) {
-//    const card = nameArrow.find(item => item.name.toLowerCase() === name.toLowerCase())
-//     console.log(card )
-  
-// }
-
-
-const fetchCountries =(name)=> {
- return fetch(`https://restcountries.com/v3.1/name/${name}`)
+export default
+    function fetchCountries(name) {
+ return fetch(`${BASE_URL}/v3.1/name/${name}?fields=flags,name,capital,population,languages`)
     .then((response) =>   response.json())
 }; 
     
-//  fetchCountries('Peru').then(renderListCantries) .catch((error)=> console.log('error'));
-// fetchCountries(name).then( renderListCards) .catch((error)=> console.log('error'));
-// function renderListCantries(nameArrow) {
-//   const countryObject = nameArrow[0]
-  
-//   const capital = countryObject.capital[0];
-//   const population = city.population;
-//   const languages = Object.values(city.languages).join(', ');
-//   const name = city.name.official;
-//   const flag = city.flags.svg;
-
-//   console.log(capital);
-//   console.log(city);
-//   console.log( name)
-//   return city({ capital }) => {
-
-//         return `
-//  <h2>  <img
-//       src="${flag}"
-//       alt="${name}"
-//     />${name} </h2>
-//   <p>Capital:'${ capital }'</p>
-//   <p>Population:${population}</p>
-//   <p>Languages:${languages}</p>
-
-//     `
-//     }).join('');
-
-// };
-
-// refs.countryList.insertAdjacentHTML('beforeend',renderListCantries)
-
-
-// function renderListCards(name, nameArrow) {
-//    const card = nameArrow.find(item => item.name.toLowerCase() === name.toLowerCase())
-//     console.log(card )
-  
-// }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import './css/styles.css';
-import fetchCountries from './fetchCountries'
+import fetchCountries from './fetchCountries';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+var _ = require('lodash');
 
-var debounce = require('lodash.debounce');
 
 
 const DEBOUNCE_DELAY = 300;
@@ -13,49 +13,63 @@ const refs = {
 }
 
 
-
-console.log(debounce)
-document.addEventListener(
-  "scroll",
-  _.debounce(() => {
-    console.log("Scroll handler call after 300ms pause");
-  }, 300)
-);
-
-// refs.onInput.addEventListener('input', _.debounce(onInput,DEBOUNCE_DELAY));
+refs.onInput.addEventListener('input', _.debounce(onInput,DEBOUNCE_DELAY));
 
 function onInput(evt) {
-  console.log(evt.currentTarget.value);
-  // return (Notify.info("Too many matches found. Please enter a more specific name."));
-   // console.log(Notify.failure("Oops, there is no country with that name")) 
+  const currentCountry = evt.target.value.trim();
+  
+  fetchCountries(currentCountry).then((contriesArray) => {
+  
+    if (contriesArray.length > 10) {
+      refs.countryList.innerHTML = '';
+       refs.countryInfo.innerHTML = '';
+       (Notify.info("Too many matches found. Please enter a more specific name."));
+    }
+    if (contriesArray.length >= 2 && contriesArray.length <= 10) {
+
+      renderCountriesItem(contriesArray);
+    }
+    if (contriesArray.length === 1) {
+  refs.countryList.innerHTML = '';
+      renderCountryInfo(contriesArray);
+    } 
+    
+  }).catch((error) => (Notify.failure("Oops, there is no country with that name"))
+  )
 }
 
-//  fetchCountries('Peru').then(renderListCantries) .catch((error)=> console.log('error'));
+function renderCountriesItem(contriesArray) {
+    refs.countryInfo.innerHTML = '';
+refs.countryList.innerHTML = '';
+ return contriesArray.forEach(element => {
+    const name = element.name.official;
+    const flag = element.flags.svg;
+    const markup = `
+    <h3><img src=${flag} width = 40> ${name}</h3>
+`;
+   refs.countryList.insertAdjacentHTML('beforeend',markup)
+  });
+}
 
-//  function renderListCantries(nameArrow) {
-//   const countryObject = nameArrow[0]
-  
-//   const capital = countryObject.capital[0];
-//   const population = city.population;
-//   const languages = Object.values(city.languages).join(', ');
-//   const name = city.name.official;
-//   const flag = city.flags.svg;
+function renderCountryInfo(contriesArray) {
+  refs.countryInfo.innerHTML = '';
+ refs.countryList.innerHTML = '';
+  return contriesArray.forEach(element => {
+    const name = element.name.official;
+    const capital = element.capital[0];
+    const population = element.population;
+    const flag = element.flags.svg;
+    const language = Object.values(element.languages).join(', ');
+    const markup = `
+    <h2><img src=${flag} width = 80> ${name}</h2>
+    <ul>
+    <li><p>Capital: ${capital}</p></li>
+    <li><p>Population: ${population}</p></li>
+    <li><p>Languages: ${language}</p></li>
+    </ul>
+`;
+    refs.countryInfo.insertAdjacentHTML('beforeend', markup);
+   
+});
+}
 
-//   console.log(capital);
-//   console.log(city);
-//   console.log( name)
-//   return city({ capital }) => {
-
-//         return `
-//  <h2>  <img
-//       src="${flag}"
-//       alt="${name}"
-//     />${name} </h2>
-//   <p>Capital:'${ capital }'</p>
-//   <p>Population:${population}</p>
-//   <p>Languages:${languages}</p>
-
-//     `
-//     }).join('');
-
-// };
